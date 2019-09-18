2204ed319210272f227636ee0b0f82831565f60d
Angom Naomi <angomnd@gmail.com>
2019-09-18 23:40:56 +0300

    Modified

--- variables.js
+++ variables.js
@@ -1,17 +1,21 @@
-//<script src="variables.js"></script>
-var x=console.log("Enter your year of birth");
-let year= Number(x);
-//let age=2019-x;
-function age(x){
+function birthday(x){
+let age=2019-x;
 if(age<18){
-    console.log("You are a minor.");
+    return ("You are a minor.");
 }
 else if(age>=18 && age<=36){
-    console.log("You are a youth.");
+    return ("You are a youth.");
 }
 else{
-    console.log("You are an elder.");
+    return ("You are an elder.");
 }
-return 2019-x;
 }
-console.log(age);
+let x=console.log("Enter your year of birth");
+//console.log(birthday);
+
+
+
+
+
+
+
